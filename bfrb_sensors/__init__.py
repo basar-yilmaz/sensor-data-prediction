9750a9f8121ea71847d14dfc6d1bc8918623a3ed
Başar Yılmaz <basar.metin2@gmail.com>
2026-05-28 18:05:07 +0300

    chore: bootstrap bfrb-sensors package with uv

---
+++ bfrb_sensors/__init__.py
@@ -0,0 +1,3 @@
+"""BFRB detection from wrist-worn multimodal sensors."""
+
+__version__ = "0.1.0"
---
+++ bfrb_sensors/data/__init__.py
@@ -0,0 +1,1 @@
+"""Data acquisition, preparation, and Lightning DataModule for the BFRB pipeline."""
